c1c7d3595bf2b1b0351e9a76f484d729ba5ad93a
Louise Koustrup <loko@tv2fyn.dk>
2023-04-03 11:32:17 +0200

    chatgpt 4 js update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const titleInput = document.querySelector('#title');
 const linkInput = document.querySelector('#link');
 const summaryInput = document.querySelector('#summary');
 const linksList = document.querySelector('#links-list');
+const toggleArchivedBtn = document.querySelector('#toggle-archived');
 
 // Create an empty array to hold entries
 let entries = [];
@@ -11,71 +12,40 @@ let entries = [];
 // Check if there are entries in localStorage
 if (localStorage.getItem('entries')) {
   entries = JSON.parse(localStorage.getItem('entries'));
+  entries = entries.map(entry => ({ ...entry, archived: entry.archived || false }));
 }
 
 // Function to add entry
 function addEntry(event) {
-  // Prevent the form from submitting and reloading the page
   event.preventDefault();
 
-  // Get the input values and create an entry object
   const title = titleInput.value;
   const link = linkInput.value;
   const summary = summaryInput.value;
-  const entry = { title, link, summary };
+  const entry = { title, link, summary, archived: false };
 
-  // Add the entry to the entries array
   entries.push(entry);
-
-  // Sort the entries array alphabetically by title
   entries.sort((a, b) => a.title.localeCompare(b.title));
 
-  // Clear the form inputs
   titleInput.value = '';
   linkInput.value = '';
   summaryInput.value = '';
 
-  // Clear the links list
-  linksList.innerHTML = '';
+  displayEntries(false);
 
-  // Add each entry to the links list
-  entries.forEach(entry => {
-    const listItem = document.createElement('li');
-    const linkTitle = document.createElement('h3');
-    const linkLink = document.createElement('a');
-    const linkSummary = document.createElement('p');
-
-    linkTitle.textContent = entry.title;
-    linkTitle.classList.add('text-lg', 'font-bold', 'mb-1');
-    linkLink.textContent = entry.link;
-    linkLink.href = entry.link;
-    linkLink.target = '_blank';
-    linkLink.classList.add('text-blue-500', 'underline', 'hover:text-blue-700');
-    linkSummary.textContent = entry.summary;
-    linkSummary.classList.add('text-gray-400');
-
-    listItem.appendChild(linkTitle);
-    listItem.appendChild(linkLink);
-    listItem.appendChild(linkSummary);
-
-    linksList.appendChild(listItem);
-  });
-
-  // Save the entries array to localStorage
   localStorage.setItem('entries', JSON.stringify(entries));
 }
 
-// Add an event listener to the form
-form.addEventListener('submit', addEntry);
+function displayEntries(showArchived) {
+  const filteredEntries = entries.filter(entry => entry.archived === showArchived);
+  linksList.innerHTML = '';
 
-// Show the entries from local storage on page load
-window.addEventListener('load', () => {
-  entries = JSON.parse(localStorage.getItem('entries')) || [];
-  entries.forEach(entry => {
+  filteredEntries.forEach((entry, index) => {
     const listItem = document.createElement('li');
     const linkTitle = document.createElement('h3');
     const linkLink = document.createElement('a');
     const linkSummary = document.createElement('p');
+    const archiveButton = document.createElement('button');
 
     linkTitle.textContent = entry.title;
     linkTitle.classList.add('text-lg', 'font-bold', 'mb-1');
@@ -85,58 +55,35 @@ window.addEventListener('load', () => {
     linkLink.classList.add('text-blue-500', 'underline', 'hover:text-blue-700');
     linkSummary.textContent = entry.summary;
     linkSummary.classList.add('text-gray-400');
+    archiveButton.textContent = entry.archived ? 'Unarchive' : 'Archive';
+    archiveButton.classList.add('bg-blue-500', 'hover:bg-blue-700', 'text-white', 'font-bold', 'py-1', 'px-2', 'rounded', 'focus:outline-none', 'focus:shadow-outline', 'ml-2');
+    archiveButton.onclick = () => {
+      archiveEntry(index);
+    };
 
     listItem.appendChild(linkTitle);
     listItem.appendChild(linkLink);
     listItem.appendChild(linkSummary);
+    listItem.appendChild(archiveButton);
 
     linksList.appendChild(listItem);
   });
-});
-
-// Add a new property 'archived' to each entry object
-entries = entries.map(entry => ({ ...entry, archived: false }));
-
-// Function to toggle archived entries
-function toggleArchivedEntries() {
-  const toggleArchivedBtn = document.querySelector("#toggle-archived");
-  const showingArchived = toggleArchivedBtn.textContent === "Hide Archived Entries";
-
-  toggleArchivedBtn.textContent = showingArchived ? "Show Archived Entries" : "Hide Archived Entries";
-  displayEntries(showingArchived);
 }
 
-// Function to archive an entry
 function archiveEntry(index) {
   entries[index].archived = !entries[index].archived;
-  localStorage.setItem("entries", JSON.stringify(entries));
-  displayEntries(document.querySelector("#toggle-archived").textContent === "Hide Archived Entries");
+  localStorage.setItem('entries', JSON.stringify(entries));
+  displayEntries(toggleArchivedBtn.textContent === 'Hide Archived Entries');
 }
 
-// Update the 'addEntry' function
-function addEntry(event) {
-  // Add your code here
-
-  // Add the 'archived' property to the new entry object
-  const entry = { title, link, summary, archived: false };
-
-  // Add your code here
+function toggleArchivedEntries() {
+  const showingArchived = toggleArchivedBtn.textContent === 'Hide Archived Entries';
+  toggleArchivedBtn.textContent = showingArchived ? 'Show Archived Entries' : 'Hide Archived Entries';
+  displayEntries(!showingArchived);
 }
 
-// Create a new function 'displayEntries'
-function displayEntries(showArchived) {
-  // Add your code here
+form.addEventListener('submit', addEntry);
 
-  // Filter the entries based on their 'archived' property
-  const filteredEntries = entries.filter(entry => entry.archived === showArchived);
-
-  // Add your code here
-}
-
-// Update the event listener for the 'load' event
-window.addEventListener("load", () => {
-  // Add your code here
-
-  // Call the 'displayEntries' function instead of duplicating the code
+window.addEventListener('load', () => {
   displayEntries(false);
 });
