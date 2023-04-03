7617d2bac20b72b3546ef9575092592973adbfb6
Louise Koustrup <loko@tv2fyn.dk>
2023-04-03 10:25:07 +0200

    test

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # digitalnotebookproject
+
+This entire project was made for me to expand my coding abilities. I have been taking multiple courses in Javascript. But instead of making this from the bottom myself I have been using ChatGPT to see, how fast and how far it could take me with this idea.
+
+This is therefore both an experiment for me in Javascript but also as to see, how far I can get with ChatGPT.
+
+I'll put the milestones down here for me and others to see, how fast it went.
+
+3/4-2023: 30 minutes
+Index.html, script.js and style.css
---
+++ script.js
@@ -0,0 +1,69 @@
+// Select elements
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title');
+const linkInput = document.querySelector('#link');
+const summaryInput = document.querySelector('#summary');
+const linksList = document.querySelector('#links-list');
+
+// Create an empty array to hold entries
+let entries = [];
+
+// Check if there are entries in localStorage
+if (localStorage.getItem('entries')) {
+  entries = JSON.parse(localStorage.getItem('entries'));
+}
+
+// Function to add entry
+function addEntry(event) {
+  // Prevent the form from submitting and reloading the page
+  event.preventDefault();
+
+  // Get the input values and create an entry object
+  const title = titleInput.value;
+  const link = linkInput.value;
+  const summary = summaryInput.value;
+  const entry = { title, link, summary };
+
+  // Add the entry to the entries array
+  entries.push(entry);
+
+  // Sort the entries array alphabetically by title
+  entries.sort((a, b) => a.title.localeCompare(b.title));
+
+  // Clear the form inputs
+  titleInput.value = '';
+  linkInput.value = '';
+  summaryInput.value = '';
+
+  // Clear the links list
+  linksList.innerHTML = '';
+
+  // Add each entry to the links list
+  entries.forEach(entry => {
+    const listItem = document.createElement('li');
+    const linkTitle = document.createElement('h3');
+    const linkLink = document.createElement('a');
+    const linkSummary = document.createElement('p');
+
+    linkTitle.textContent = entry.title;
+    linkTitle.classList.add('text-lg', 'font-bold', 'mb-1');
+    linkLink.textContent = entry.link;
+    linkLink.href = entry.link;
+    linkLink.target = '_blank';
+    linkLink.classList.add('text-blue-500', 'underline', 'hover:text-blue-700');
+    linkSummary.textContent = entry.summary;
+    linkSummary.classList.add('text-gray-400');
+
+    listItem.appendChild(linkTitle);
+    listItem.appendChild(linkLink);
+    listItem.appendChild(linkSummary);
+
+    linksList.appendChild(listItem);
+  });
+
+  // Save the entries array to localStorage
+  localStorage.setItem('entries', JSON.stringify(entries));
+}
+
+// Add an event listener to the form
+form.addEventListener('submit', addEntry);
